5ed53b890dd8a1e23630c53a772740c01e42e789
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-02-19 12:01:32 +0100

    testing push

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ func sayHy(name string) {
 	fmt.Printf("Hello dear friend your name is: %v\n", name)
 }
 func hello() {
-	fmt.Println("hello from my function")
+	fmt.Println("hello from my function testging update")
 }
 func main() {
 	fmt.Println("Hello world from golang")
