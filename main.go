429d57e66167bcd9c08daee2197e7cebf6dd9b75
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-02-19 11:59:04 +0100

    fixed golang problems

--- main.go
+++ main.go
@@ -5,8 +5,11 @@ import "fmt"
 func sayHy(name string) {
 	fmt.Printf("Hello dear friend your name is: %v\n", name)
 }
-
+func hello() {
+	fmt.Println("hello from my function")
+}
 func main() {
 	fmt.Println("Hello world from golang")
 	sayHy("Advan")
+	hello()
 }
